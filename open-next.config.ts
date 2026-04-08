b269b8e4429731426a5ab89943b92934374cef6c
Tannysoft <tannysoft@gmail.com>
2026-04-09 04:06:17 +0700

    update cache kv

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,8 +2,8 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
 // To enable ISR caching on Cloudflare, uncomment the lines below
 // and add a KV namespace "NEXT_INC_CACHE_KV" in wrangler.jsonc.
-// import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
 export default defineCloudflareConfig({
-  // incrementalCache: kvIncrementalCache,
+  incrementalCache: kvIncrementalCache,
 });
